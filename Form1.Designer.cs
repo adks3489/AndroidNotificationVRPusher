43931a88c9982ae73b5c759ba2d1a480e33a7091
James Chiang <adks3489@gmail.com>
2016-05-02 10:26:44 +0800

    basic function

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AndroidNotificationVRPusher {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnStartVR = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(14, 12);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(121, 22);
+            this.txtServerIP.TabIndex = 1;
+            this.txtServerIP.Text = "192.168.1.104";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(141, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "連線";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.AutoSize = true;
+            this.txtStatus.Location = new System.Drawing.Point(14, 43);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(41, 12);
+            this.txtStatus.TabIndex = 3;
+            this.txtStatus.Text = "未連線";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(16, 68);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(369, 188);
+            this.txtLog.TabIndex = 4;
+            // 
+            // btnStartVR
+            // 
+            this.btnStartVR.Enabled = false;
+            this.btnStartVR.Location = new System.Drawing.Point(310, 39);
+            this.btnStartVR.Name = "btnStartVR";
+            this.btnStartVR.Size = new System.Drawing.Size(75, 23);
+            this.btnStartVR.TabIndex = 5;
+            this.btnStartVR.Text = "啟動VR";
+            this.btnStartVR.UseVisualStyleBackColor = true;
+            this.btnStartVR.Click += new System.EventHandler(this.btnStartVR_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 264);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(397, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(125, 17);
+            this.toolStripStatusLabel1.Text = "Created By adks3489";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 286);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnStartVR);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Android Noticiation VRPusher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtServerIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label txtStatus;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnStartVR;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using Valve.VR;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace AndroidNotificationVRPusher {
+    public partial class Form1 : Form {
+        int m_nNotifyTime;
+        HmdMatrix34_t m_PosMatrix;
+        SolidBrush m_TextBrush;
+        SolidBrush m_BackgroundBrush;
+        int m_nFontSize;
+
+        CVRSystem m_HMD;
+        ulong m_ulOverlayHandle;
+
+        bool m_bRunning;
+        TcpClient server;
+        StreamReader serverStream;
+
+        public Form1() {
+            InitializeComponent();
+            LoadConfig();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) {
+            if( !Init() ) {
+                btnStartVR.Enabled = true;
+            }
+            if (ConfigurationManager.AppSettings["AutoConnect"] == "1") {
+                SetTimeout(() => {
+                    ConnectServer();
+                }, 500);                
+            }
+        }
+
+        public void LoadConfig() {
+            m_nNotifyTime = Convert.ToInt32(ConfigurationManager.AppSettings["NotifyTime"]);
+            string[] matrix = ConfigurationManager.AppSettings["NotificationPositionMatrix"].Split(',');
+            m_PosMatrix.m0 = (float)Convert.ToDouble(matrix[0]);
+            m_PosMatrix.m1 = (float)Convert.ToDouble(matrix[1]);
+            m_PosMatrix.m2 = (float)Convert.ToDouble(matrix[2]);
+            m_PosMatrix.m3 = (float)Convert.ToDouble(matrix[3]);
+            m_PosMatrix.m4 = (float)Convert.ToDouble(matrix[4]);
+            m_PosMatrix.m5 = (float)Convert.ToDouble(matrix[5]);
+            m_PosMatrix.m6 = (float)Convert.ToDouble(matrix[6]);
+            m_PosMatrix.m7 = (float)Convert.ToDouble(matrix[7]);
+            m_PosMatrix.m8 = (float)Convert.ToDouble(matrix[8]);
+            m_PosMatrix.m9 = (float)Convert.ToDouble(matrix[9]);
+            m_PosMatrix.m10 = (float)Convert.ToDouble(matrix[10]);
+            m_PosMatrix.m11 = (float)Convert.ToDouble(matrix[11]);
+
+            txtServerIP.Text = ConfigurationManager.AppSettings["DefaultIP"];
+
+            m_TextBrush = new SolidBrush(Color.FromArgb(Convert.ToInt32(ConfigurationManager.AppSettings["TextColor"].ToString(), 16)));            
+            m_BackgroundBrush = new SolidBrush(Color.FromArgb(Convert.ToInt32(ConfigurationManager.AppSettings["BackgroundColor"].ToString(), 16)));            
+            m_nFontSize = Convert.ToInt32(ConfigurationManager.AppSettings["FontSize"]);
+        }
+
+        public bool Init() {
+            EVRInitError peError = EVRInitError.None;
+            m_HMD = OpenVR.Init(ref peError, EVRApplicationType.VRApplication_Overlay);
+            if(peError != EVRInitError.None) {
+                txtLog.AppendText("OpenVR啟動失敗: " + peError + '\n');
+                return false;
+            }
+            if (OpenVR.Overlay != null) {
+                EVROverlayError overlayError = OpenVR.Overlay.CreateOverlay("AndroidNotificationVRPusher", "AndroidNotifier", ref m_ulOverlayHandle);
+                if (overlayError == EVROverlayError.None) {
+                    OpenVR.Overlay.SetOverlayWidthInMeters(m_ulOverlayHandle, 1f);
+                    OpenVR.Overlay.SetOverlayTransformTrackedDeviceRelative(m_ulOverlayHandle, OpenVR.k_unTrackedDeviceIndex_Hmd, ref m_PosMatrix);
+                    OpenVR.Overlay.SetOverlayAlpha(m_ulOverlayHandle, 0.7f);
+                    txtLog.AppendText("OpenVR啟動成功\n");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void ShutDown() {
+            m_bRunning = false;            
+            if (server != null && server.Connected) {
+                serverStream.Close();
+                server.Close();
+            }
+            OpenVR.Shutdown();
+        }
+
+        private void btnStartVR_Click(object sender, EventArgs e) {
+            btnStartVR.Enabled = false;
+            if( !Init()) {
+                btnStartVR.Enabled = true;
+            }
+        }
+
+        int count = 0;
+        private void button1_Click(object sender, EventArgs e) {
+            ShowNotification("Test Msg "+count++);
+        }
+
+        void ShowNotification(string msg) {
+            string strImgPath = Directory.GetCurrentDirectory() + "\\Msg.png";
+            Bitmap bitmap = new Bitmap(1024, 512);
+            Graphics g = Graphics.FromImage(bitmap);
+            Font textFont = new Font("Arial", m_nFontSize);
+            SizeF textSize = g.MeasureString(msg, textFont);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g.FillRectangle(m_BackgroundBrush, new Rectangle(0, 0, (int)textSize.Width+20, (int)textSize.Height+20));
+            RectangleF rectf = new RectangleF(10, 10, textSize.Width, textSize.Height);
+            g.DrawString(msg, textFont, m_TextBrush, rectf);
+            g.Flush();
+            bitmap.Save(strImgPath);
+            OpenVR.Overlay.SetOverlayFromFile(m_ulOverlayHandle, strImgPath);
+            OpenVR.Overlay.ShowOverlay(m_ulOverlayHandle);
+
+            ClearAllTimeout();
+            SetTimeout(() => {
+                OpenVR.Overlay.HideOverlay(m_ulOverlayHandle);
+            }, m_nNotifyTime);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e) {
+            ConnectServer();
+        }
+
+        void ConnectServer() {
+            try {
+                server = new TcpClient(txtServerIP.Text, 17749);
+                serverStream = new StreamReader(server.GetStream(), Encoding.UTF8);
+                Thread thread = new Thread(ReceiveTask);
+                thread.Start();
+                m_bRunning = true;
+                this.Invoke((MethodInvoker)delegate {
+                    txtStatus.Text = "已連線";
+                });
+            }
+            catch (SocketException ex) {
+                this.Invoke((MethodInvoker)delegate {
+                    txtStatus.Text = "連線失敗";
+                });
+            }
+        }
+
+        void ReceiveTask() {
+            while (m_bRunning) {
+                if (server.Connected) {
+                    try {
+                        string s = serverStream.ReadLine();
+                        if(s.Length > 2) {
+                            s = s.Substring(2);
+                            Console.WriteLine(s);
+                            this.Invoke((MethodInvoker)delegate {
+                                txtLog.AppendText("收到通知: " + s + '\n');
+                            });
+                            ShowNotification(s.Replace(';','\n'));
+                        }
+                    }
+                    catch(Exception ex) {
+                        this.Invoke((MethodInvoker)delegate {
+                            txtStatus.Text = "未連線";
+                        });
+                        ConnectServer();
+                    }
+                }
+                else {
+                    this.Invoke((MethodInvoker)delegate {
+                        txtStatus.Text = "未連線";
+                    });
+                    ConnectServer();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            ShutDown();
+        }
+
+
+        #region SetTimeout/ClearTimeout Simulation
+        static Dictionary<Guid, Thread> _setTimeoutHandles =
+            new Dictionary<Guid, Thread>();
+        static Guid SetTimeout(Action cb, int delay) {
+            return SetTimeout(cb, delay, null);
+        }
+        static Guid SetTimeout(Action cb, int delay, Form uiForm) {
+            Guid g = Guid.NewGuid();
+            Thread t = new Thread(() => {
+                Thread.Sleep(delay);
+                _setTimeoutHandles.Remove(g);
+                if (uiForm != null)
+                    uiForm.Invoke(cb);
+                else
+                    cb();
+            });
+            _setTimeoutHandles.Add(g, t);
+            t.Start();
+            return g;
+        }
+        static void ClearTimeout(Guid g) {
+            if (!_setTimeoutHandles.ContainsKey(g))
+                return;
+            _setTimeoutHandles[g].Abort();
+            _setTimeoutHandles.Remove(g);
+        }
+        static void ClearAllTimeout() {
+            foreach(var g in _setTimeoutHandles) {
+                g.Value.Abort();
+            }
+            _setTimeoutHandles.Clear();
+        }
+        #endregion
+    }
+}
