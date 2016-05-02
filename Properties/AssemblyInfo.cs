69811ed208665badd75a3d5f7fc1528877cf401e
James Chiang <adks3489@gmail.com>
2016-05-02 10:31:22 +0800

    Add info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AndroidNotificationVRPusher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/adks3489/AndroidNotificationVRPusher")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("adks3489 (James Chiang)")]
 [assembly: AssemblyProduct("AndroidNotificationVRPusher")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
